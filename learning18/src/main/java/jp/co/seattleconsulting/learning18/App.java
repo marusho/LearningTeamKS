a68eece81b312ec6badfbe0a49090e0ca250c576
shoma <shoma@DESKTOP-R9691QT>
2018-08-23 22:21:07 +0900

    サンプルソースコード追加
    JavaDoc追加

--- learning18/src/main/java/jp/co/seattleconsulting/learning18/App.java
+++ learning18/src/main/java/jp/co/seattleconsulting/learning18/App.java
@@ -2,12 +2,20 @@ package jp.co.seattleconsulting.learning18;
 
 import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
- * Hello world!
+ * Appデフォルト作成クラス
  *
  */
 public class App
 {
+	/**
+	 * Mainメソッド
+	 * サンプルソース群
+	 *
+	 * @param args 初期設定引数
+	 */
     @SuppressWarnings("resource")
 	public static void main( String[] args )
     {
@@ -17,5 +25,42 @@ public class App
         System.out.println("あなたの年齢を入力してください。");
         int age = scanner.nextInt();
         System.out.println("ようこそ" + age + "才の" + name + "さん");
+
+        if (StringUtils.isEmpty("")) {
+
+        }
+
+        String s = "３";
+
+        System.out.println(org.apache.commons.lang.StringUtils.isNumeric(s));
+        System.out.println(org.apache.commons.lang3.StringUtils.isNumeric(s));
+
+        System.out.println(org.apache.commons.lang.math.NumberUtils.isDigits(s));
+        System.out.println(org.apache.commons.lang.math.NumberUtils.isNumber(s));
+
+        System.out.println(org.apache.commons.lang3.math.NumberUtils.isDigits(s));
+        System.out.println(org.apache.commons.lang3.math.NumberUtils.isNumber(s));
+
+        s = "-100";
+        System.out.println(org.apache.commons.lang.StringUtils.isNumeric(s));
+        System.out.println(org.apache.commons.lang3.StringUtils.isNumeric(s));
+
+        System.out.println(org.apache.commons.lang.math.NumberUtils.isDigits(s));
+        System.out.println(org.apache.commons.lang.math.NumberUtils.isNumber(s));
+
+        System.out.println(org.apache.commons.lang3.math.NumberUtils.isDigits(s));
+        System.out.println(org.apache.commons.lang3.math.NumberUtils.isNumber(s));
+
+        s = "0xff";
+        System.out.println(org.apache.commons.lang.StringUtils.isNumeric(s));
+        System.out.println(org.apache.commons.lang3.StringUtils.isNumeric(s));
+
+        System.out.println(org.apache.commons.lang.math.NumberUtils.isDigits(s));
+        System.out.println(org.apache.commons.lang.math.NumberUtils.isNumber(s));
+
+        System.out.println(org.apache.commons.lang3.math.NumberUtils.isDigits(s));
+        System.out.println(org.apache.commons.lang3.math.NumberUtils.isNumber(s));
+
+
     }
 }
